250549cf5bfc0324978acebb5d73aba839b7b568
florenjohanson <flore.johanson@gmail.com>
2024-08-15 14:32:28 -0300

    flo

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -6,17 +6,28 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
+const form = document.getElementById('myForm');
+const password1 = document.getElementById('password1');
+const password2 = document.getElementById('password2');
+const alertSuccess = document.getElementById('alert-success');
+const alertDanger = document.getElementById('alert-danger');
 
-const colsm6 = document.getElementById('myForm');
+const regBtn = document.getElementById('regBtn');
 
-colsm6.addEventListener ('submit', (event) => {
-    const password1 = document.getElementById('password1').value;
-    const password2 = document.getElementById('password2').value;   
+regBtn.addEventListener('click', (event) => {
+  event.preventDefault(); 
 
+  const passwordValue1 = password1.value;
+  const passwordValue2 = password2.value;
 
-    if (password1 !== password2) {
-        alert('Las contraseñas no coinciden.');
-        event.preventDefault();
-} 
-
+  if (passwordValue1 === passwordValue2) {
+    
+    alertSuccess.classList.remove('d-none'); 
+    alertDanger.classList.add('d-none'); 
+    
+  } else {
+    
+    alertDanger.classList.remove('d-none');
+    alertSuccess.classList.add('d-none'); 
+  }
 });
