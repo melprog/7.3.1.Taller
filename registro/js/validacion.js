08af64ff860635d2a5608ed1043c8e046af4fda9
florenjohanson <flore.johanson@gmail.com>
2024-08-15 14:00:59 -0300

    solicitud contraseña igual

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,18 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+const colsm6 = document.getElementById('myForm');
+
+colsm6.addEventListener ('submit', (event) => {
+    const password1 = document.getElementById('password1').value;
+    const password2 = document.getElementById('password2').value;   
+
+
+    if (password1 !== password2) {
+        alert('Las contraseñas no coinciden.');
+        event.preventDefault();
+} 
+
+});
